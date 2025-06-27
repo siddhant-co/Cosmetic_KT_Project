148d930484a58f67b0f753e8e137e4c8d4d84b7c
siddhant-co <your-new-email@example.com>
2025-06-27 10:51:07 +0530

    "testimonial changes"

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -2,18 +2,11 @@ import { fetchBanners } from "./function";
 import CategorySection from "@/components/CategorySection/CategorySection";
 import HotListWrapper from "@/components/HotList/HotListWrapper";
 import TestProductCard from "@/components/ProductCard/TestProductCard";
-import TestimonialsSection from "@/components/TestimonialsSection/TestimonialsSection";
-<<<<<<< HEAD
-import TopCategories from "@/components/TopCategories/TopCategories";
-
-
-
-
-
-=======
 import FeaturesBanner from "@/components/ServersideComponent/FeaturesBanner/FeaturesBanner";
 import HeroBanner from "@/components/ServersideComponent/HeroBanner/HeroBanner";
->>>>>>> 521e6dcf0bac058889019357ef595b4045246fa0
+import TestimonialsSection from "@/components/TestimonialsSection/TestimonialsSection";
+import TopCategories from "@/components/TopCategories/TopCategories";
+
 
 export default async function HomePage() {
   const banners = await fetchBanners(); // Server-side
